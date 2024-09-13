668076f421d4007c004ed1781b818fd37106feda
fazal <fazalahamed628@gmail.com>
2024-09-13 19:18:32 +0530

    fix:access token problem fixed

--- server.ts
+++ server.ts
@@ -31,7 +31,7 @@ app.use(cors({
     origin: 'https://article-nest-client-in.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'custom-header'],
    
 }));
 
